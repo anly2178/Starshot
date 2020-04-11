0b8690001d86a4ef4c46cc4724623f5b82fcb93b
Andrew Ly <anly2178@uni.sydney.edu.au>
2020-04-11 12:24:41 +1000

    Generalised the intensity plotting function

--- frac_power.py
+++ frac_power.py
@@ -1,7 +1,6 @@
 import numpy as np
-from scipy import special
-from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
+from scipy.special import erf
 
 np.seterr(all='ignore', invalid='ignore')
 
@@ -15,10 +14,9 @@ def find_total_relative_energy(target_beta, reflectivity):
     Function calculates the relative energy summed over all incident photons.
     Assumes that photons are either absorbed or reflected, not transmitted.
     """
-    beta = target_beta
+    B = target_beta
     R = reflectivity
-    return (-(1+R)*(1-beta)+np.sqrt(8*beta*R*(1-beta)+((1-beta)**2) * \
-    ((1+R)**2)))/(4*R*(1-beta))
+    return (-(1+R)*(1-B)+np.sqrt(8*B*R*(1-B)+((1-B)**2) * ((1+R)**2)))/(4*R*(1-B))
 
 def find_firing_time(total_mass, surface_area, absorptivity, total_rel_energy,\
     max_temp, emissivity):
@@ -48,46 +46,61 @@ def find_beam_width(dist, wavelength, transmitter_D):
 def find_fraction_incident(surface_area, beam_width):
     """
     Finds fraction of laser power incident on the sail.
-    Assumes circular Gaussian beam.
+    Assumes circular Gaussian beam and projection of sail is circular.
     """
-    return (special.erf(np.sqrt(surface_area / 2*np.pi) / beam_width))**2
+    return (erf(np.sqrt(surface_area / 2*np.pi) / beam_width))**2
+
 #
-#The code below is incorrect because, for some reason, I assumed that the diameter
-#of the transmitter is the same as the beam waist. It is actually assumed to be
-#far from the beam waist. In order to find the intensity distribution, I require
-#at least the peak intensity at the transmitter.
-#
-# def find_peak_intensity(laser_power, beam_width):
+#The peak intensity is wrong for some reason. I essentially found a limit
+#by considering a really small area. However, the peak intensity is such, that
+#if you graphed the intensity distribution, the total power (area under the graph)
+#is greater than the laser power.
+# def find_peak_intensity(laser_power, dist, wavelength, transmitter_D):
 #     """
 #     Finds the peak intensity of the circular Gaussian beam.
 #     """
+#     beam_width = find_beam_width(dist, wavelength, transmitter_D)
+#     peak_I = laser_power * find_fraction_incident(1e-10, beam_width)/1e-10
+#     return peak_I
+# #
+# print(find_peak_intensity(500e9, 100000000, 650e-9,10))
 #
-#     return 2 * laser_power / (np.pi * beam_width ** 2)
-#
-# def plot_intensity(distance, wavelength, diameter, laser_power):
-#     """
-#     Plots the radial intensity pattern at a distance away from the transmitter.
-#     Plots from x = -5 to x = 5 since the sail is likely not going to have a
-#     diameter of over 10m.
-#     Accepts distances at least 10000 m, as this is where diffraction effects
-#     are appreciable.
-#     """
-#     if distance < 10000:
-#         print("Distance must be at least 10000m for diffraction to be appreciable")
-#         return None
-#     beam_width = find_beam_width(distance, wavelength, diameter)
-#     peak_I = find_peak_intensity(laser_power, beam_width)
-#
-#     radius = np.linspace(-5,5,200)
-#     i = 0
-#     intensity = []
-#     while i < len(radius):
-#         r = radius[i]
-#         I = peak_I * (diameter / beam_width)**2 * np.exp(-2 * r**2 / beam_width**2)
-#         intensity.append(I)
-#         i += 1
-#     plt.plot(radius,intensity)
-#     return None
-#
-# plot_intensity(10000000, 650e-9, 10, 500e9)
-# plt.show()
+
+#You could easily change this to show the intensity distribution if you have the peak intensity.
+def plot_intensity(dist, wavelength, transmitter_D):
+    """
+    Plots the radial intensity/peak_intensity pattern at a distance away from the transmitter.
+    Plots from x = -5 to x = 5 since the sail is likely not going to have a
+    diameter of over 10m.
+    Accepts distances at least 1000000 m, as otherwise it is not worthwhile considering
+    diffraction effects.
+    Function allows for user to overplot for multiple distances, however the user
+    must call plt.show() after function call.
+    """
+    if dist < 1000000:
+        print("Distance must be at least 1000 km for diffraction-limiting effects to be appreciable")
+        return None
+
+    beam_width = find_beam_width(dist, wavelength, transmitter_D)
+    # peak_I = find_peak_intensity(laser_power, dist, wavelength, transmitter_D)
+
+    radius = np.linspace(-5,5,1000)
+    i = 0
+    y = []
+    while i < len(radius):
+        r = radius[i]
+        ratio_I = np.exp(-2 * r**2 / beam_width**2)
+        y.append(ratio_I)
+        i += 1
+    plt.plot(radius,y)
+    plt.title('Radial distribution of intensity')
+    plt.ylabel(r'$\frac{I}{I_{peak}}$', fontsize = 16)
+    plt.xlabel('Position along the diameter of beam (m)')
+    return None
+
+plot_intensity(1000000, 650e-9, 10)
+plot_intensity(10000000, 650e-9, 10)
+plot_intensity(30000000, 650e-9, 10)
+plot_intensity(60000000, 650e-9, 10)
+plot_intensity(100000000, 650e-9, 10)
+plt.show()
