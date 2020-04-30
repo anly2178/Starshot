3c3dc23237de676456b0b5cd5e39da5172966d5f
Andrew Ly <anly2178@uni.sydney.edu.au>
2020-04-30 19:13:42 +1000

    Fixed Runge Kutta method

--- motion.py
+++ motion.py
@@ -19,30 +19,26 @@ def differential_eq(x, params):
     m_tot = 2*m_sail
     reflectance = params["reflectance"]
     power = params["power"]
-    #Calculates the rate of change of beta
-    fraction = find_fraction_incident(params, dist)
-    beta_dot = 2 * reflectance * power * fraction * (1-beta**2)**1.5 * (1-beta) / (m_tot * c**2 * (1+beta))
 
-    return beta_dot
+    fraction = find_fraction_incident(params, dist) #fraction of power incident
+    lor = 1/(1-beta**2)**0.5 #Lorentz factor
+    #Derivative of state with respect to time
+    beta_dot = 2 * reflectance * power * fraction * (1-beta) / (m_tot * c**2 * lor**3 * (1+beta))
+    vel = c*beta
+    xdot = np.array([beta_dot, vel])
+    return xdot
 
 def state_vs_t(params):
     """
     Returns an array of states
         First row contains the speed of sail as a fraction of the speed of light
         Second row contains the distance from the DE system
-        Third row contains the equilibrium temperature
     and an array of corresponding times.
     Assumes diffraction effects.
 
     Parameters must be defined (in SI units) and passed into function. For example:
         params = {"m_sail": 1e-3, "thickness": 1e-6, "density": 1400, "reflectance": 1, "absorptance": 9e-8,
                     "power": 1e11, "diameter": 1e4, "wavelength": 1064e-9}
-    k is a constant related to the shape of the sail.
-        k = 1 for square sail
-        k = pi / 4 for circular sail
-    alpha is a constant related to the shape of the DE system.
-        alpha = 1 for square DE system
-        alpha = 1.22 for circular DE system
     """
     c = 2.998e8
 
@@ -51,7 +47,7 @@ def state_vs_t(params):
     x0 = np.array([0,0])  #Initial state
     t0 = 0  #Initial t
     tf = 1e4  #Final t
-    dt = 1  #Initial step size
+    dt = 1  #Step size
 
     #Create arrays to fill
     t = np.append(np.linspace(0,0.8,5), np.logspace(0,4,195)) #Create time, starts off linear and transitions into logarithmic
@@ -66,15 +62,10 @@ def state_vs_t(params):
 
         #Find next beta
         k1 = dt * f(t[i], x[:,i])
-        k2 = dt * f(t[i] + dt/2, x[:,i] + np.array([k1/2,0]))
-        k3 = dt * f(t[i] + dt/2, x[:,i] + np.array([k2/2,0]))
-        k4 = dt * f(t[i] + dt, x[:,i] + np.array([k3,0]))
-        dbeta = (k1 + 2*k2 + 2*k3 + k4)/6
-        x[0,i+1] = x[0,i] + dbeta
-
-        #Find next position
-        velocity = c * (x[0,i+1] + x[0,i])/2 #Trapezoidal rule
-        dposition = velocity * dt
-        x[1,i+1] = x[1,i] + dposition
+        k2 = dt * f(t[i] + dt/2, x[:,i] + k1/2)
+        k3 = dt * f(t[i] + dt/2, x[:,i] + k2/2)
+        k4 = dt * f(t[i] + dt, x[:,i] + k3)
+        dx = (k1 + 2*k2 + 2*k3 + k4)/6
+        x[:,i+1] = x[:,i] + dx
 
     return x, t
