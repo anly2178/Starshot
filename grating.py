f4501e989ffc4a8c213ef9a6bf1b4d6ea2ff1877
Andrew Ly <anly2178@uni.sydney.edu.au>
2020-05-04 21:32:29 +1000

    Made the grating functions slightly more general. Now able to find beta,distance and time for the grating

--- grating.py
+++ grating.py
@@ -3,24 +3,24 @@ from numpy import arcsin
 
 def find_diffracted_angle(order,wavelength,period):
     """
-    Given the grating period and wavlength of light, return the angle (in degrees)
+    Given the grating period and wavlength of light, return the angle (in radians)
     from the normal for a specific order.
+    Assume normal incidence
     """
     angle = arcsin(order * wavelength / period)
-    angle = np.degrees(angle)
     return angle
 
-def find_rt_coefficients(filepath, incident):
+def find_rt_coefficients(filepath):
     """
     Input:  filepath to txt file (str)
             incident angle in degrees (int or float)
     Output: Multidimensional array of efficiencies with the form:
-                [[r-1, r0, r1, rtot],[t-1, t0, t1, ttot]]
+                [[r-1, r0, r1],[t-1, t0, t1]]
             where r-1 is the reflection coefficient for the -1 order,
             t0 is the transmission coefficient for the 0 order,
             etc.
             and rtot and ttot are the total coefficients.
-
+    Assume normal incidence
     Currently specific to the txt file created by Evan Xie, which are the
     results of 2019 Ilic.
     """
@@ -46,7 +46,7 @@ def find_rt_coefficients(filepath, incident):
         #The argument incident angle must exactly match the one in the txt file.
         #This can be changed to the closest matching incident angle, but
         #it is unnecessary for now, as we care about normal incidence.
-        if angle == incident:
+        if angle == 0:
             coeff = float(chars[1])
         else:
             continue    #Continue to next line if not a match
@@ -56,7 +56,35 @@ def find_rt_coefficients(filepath, incident):
             break
     f.close()
     #Restructure coefficients
-    reflec = [coeffs[1],coeffs[0],coeffs[2],coeffs[3]]
-    transm = [coeffs[6],coeffs[4],coeffs[5],coeffs[7]]
+    reflec = [coeffs[1],coeffs[0],coeffs[2]]
+    transm = [coeffs[6],coeffs[4],coeffs[5]]
     coeffs = [reflec,transm]
     return coeffs
+
+def find_angles_coeffs(filepath, wavelength, period):
+    """
+    Specific to results provided to me by Evan Xie.
+
+    Input: Filepath to txt file with reflection and transmission coefficients.
+    Output: Multidimensional array with angle of the order, reflection and transmission coefficients.
+                [[angle-1,r-1,t-1],[angle0,r0,t0],[angle1,r1,t1]]
+    """
+    #Find angles for each order; angles for reflection are the same as angle for transmission at normal incidence
+    orders = [-1,0,1]
+    angles = []
+    for order in orders:
+        angle = np.radians(find_diffracted_angle(order,wavelength,period))
+        angles.append(angle)
+    #Find efficiences of each reflected and transmitted order
+    coeffs = find_rt_coefficients(filepath)
+    #Match angles and coefficients
+    angles_coeffs = []
+    i = 0
+    while i < len(orders):
+        angle = angles[i]
+        r = coeffs[0][i]
+        t = coeffs[1][i]
+        angle_r_t = [angle,r,t]
+        angles_coeffs.append(angle_r_t)
+        i += 1
+    return angles_coeffs
--- motion.py
+++ motion.py
@@ -1,6 +1,8 @@
 import numpy as np
+from numpy import cos
 from .temp import find_temp
 from .laser import find_fraction_incident
+from .grating import find_rt_coefficients,find_diffracted_angle
 
 def differential_eq(x, params):
     """
@@ -27,49 +29,7 @@ def differential_eq(x, params):
     xdot = np.array([beta_dot, vel])
     return xdot
 
-def state_vs_t(params):
-    """
-    Returns an array of states
-        First row contains the speed of sail as a fraction of the speed of light
-        Second row contains the distance from the DE system
-    and an array of corresponding times.
-    Assumes diffraction effects.
-
-    Parameters must be defined (in SI units) and passed into function. For example:
-        params = {"m_sail": 1e-3, "thickness": 1e-6, "density": 1400, "reflectance": 1, "absorptance": 9e-8,
-                    "power": 1e11, "diameter": 1e4, "wavelength": 1064e-9}
-    """
-    c = 2.998e8
-
-    #Initialise conditions
-    f = lambda t, x : differential_eq(x, params) #Differential equation
-    x0 = np.array([0,0])  #Initial state
-    t0 = 0  #Initial t
-    tf = 1e4  #Final t
-    dt = 1  #Step size
-
-    #Create arrays to fill
-    t = np.append(np.linspace(0,0.8,5), np.logspace(0,4,195)) #Create time, starts off linear and transitions into logarithmic
-    nt = t.size
-    nx = x0.size
-    x = np.zeros((nx,200))
-    x[:,0] = x0 #Speed and distance
-
-    #Runge Kutta method to find states as a function of time
-    for i in range(nt - 1):
-        dt = t[i+1] - t[i]
-
-        #Find next beta
-        k1 = dt * f(t[i], x[:,i])
-        k2 = dt * f(t[i] + dt/2, x[:,i] + k1/2)
-        k3 = dt * f(t[i] + dt/2, x[:,i] + k2/2)
-        k4 = dt * f(t[i] + dt, x[:,i] + k3)
-        dx = (k1 + 2*k2 + 2*k3 + k4)/6
-        x[:,i+1] = x[:,i] + dx
-
-    return x, t
-
-def grating_differential_eq(x, params, coeffs, angles):
+def grating_differential_eq(x, params):
     """
     Defines the differential equations as in Kulkarni 2018 equation (23).
     Changed to account for reflection (and transmission) at an angle.
@@ -88,20 +48,37 @@ def grating_differential_eq(x, params, coeffs, angles):
     #Get parameters
     m_sail = params["m_sail"]
     m_tot = 2*m_sail
-    reflectance = params["reflectance"]
     power = params["power"]
+    angles_coeffs = params["angles_coeffs"]
 
-    fraction = find_fraction_incident(params, dist) #fraction of power incident
+    #Each order contributes an amount of 'power' reflected; equivalent to finding the change in momentum
+    #Add all these powers together to find the effective power
+    eff_p = 0 #Effective power reflected
+    fraction = find_fraction_incident(params, dist) #account for diffraction effects
+    p_inc = fraction*power #incident power
+    i = 0
+    while i < len(angles_coeffs):
+        angle_r_t = angles_coeffs[i]
+        angle = angle_r_t[0] #angle of order
+        r = angle_r_t[1] #reflection coefficient
+        t = angle_r_t[2] #transmission coefficient
+        fac = cos(angle) #cosine factor due to reflection/transmission at an angle
+        p_r = r*p_inc*fac #power contribution from reflection
+        p_t = t*p_inc*(1-fac) #power contribution from transmission
+        eff_p += p_r + p_t
+        i += 1
+
     lor = 1/(1-beta**2)**0.5 #Lorentz factor
     #Derivative of state with respect to time
-    beta_dot = 2 * reflectance * power * fraction * (1-beta) / (m_tot * c**2 * lor**3 * (1+beta))
+    beta_dot = 2 * eff_p * (1-beta) / (m_tot * c**2 * lor**3 * (1+beta))
     vel = c*beta
     xdot = np.array([beta_dot, vel])
     return xdot
 
-def grating_state_vs_t(params):
+
+def state_vs_t(params):
     """
-    For grating, returns an array of states
+    Returns an array of states
         First row contains the speed of sail as a fraction of the speed of light
         Second row contains the distance from the DE system
     and an array of corresponding times.
@@ -111,13 +88,14 @@ def grating_state_vs_t(params):
         params = {"m_sail": 1e-3, "thickness": 1e-6, "density": 1400, "reflectance": 1, "absorptance": 9e-8,
                     "power": 1e11, "diameter": 1e4, "wavelength": 1064e-9}
     """
+    c = 2.998e8
 
     #Initialise conditions
-    f = lambda t, x : differential_eq(x, params) #Differential equation
+    if "angles_coeffs" in params:
+        f = lambda t, x : grating_differential_eq(x, params)
+    else:
+        f = lambda t, x : differential_eq(x, params) #Differential equation
     x0 = np.array([0,0])  #Initial state
-    t0 = 0  #Initial t
-    tf = 1e4  #Final t
-    dt = 1  #Step size
 
     #Create arrays to fill
     t = np.append(np.linspace(0,0.8,5), np.logspace(0,4,195)) #Create time, starts off linear and transitions into logarithmic
