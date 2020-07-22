4816be51541168f248108e89a9ac2138820fe097
jwid8259 <63948487+jwid8259@users.noreply.github.com>
2020-07-23 00:23:07 +1000

    Added files to replace optical_constants
    Linking optical constants directly to materials as Material objects
    Materials can be saved and loaded as (.txt) files?
    Materials have a name, density and a set of optical constants
    CSVs with optical constants should be able to be read and saved as the optical constants for specific materials
    Equations that describe the constants can only be implemented manually by editing material_helpers.py and material_equations.py - not sure how to get around manual changes to code in a reasonable manner; this is the best option I can see right now BUT will eventually add a README to make the editing process easier for users

---
+++ Material/Material.py
@@ -0,0 +1,139 @@
+import os
+from material_helpers import interpolate_from_list, use_equation
+
+""" Each material should have a:
+        - name (str; potentially multiple names. Main name is chemical formula
+          e.g. SiO2, GeO2, Si3N4, etc.)
+        - density (float; kg/m^3)
+        - set of optical constants
+            * real component (list of list/tuples, each tuple has 2 parts,
+              the wavelength and the value)
+            * imaginary component (same as above)
+        - boolean value to designate whether it has equations which can describe
+          its optical constants. If this is the case, the get_n or get_k
+          functions will be altered in order to use the equations. For an equation
+          to be used, the user must code it in manually
+            (see materials_equations.py)
+"""
+
+class Material:
+
+    def __init__(self, name, density, n_list = None, k_list = None, has_equations_for_n = False, has_equations_for_k = False):
+        """ Constructor requires at least the name and the density
+        """
+        self.name = name
+        self.density = density
+        self.n_list = n_list
+        self.k_list = k_list
+        self.has_equations_for_n = has_equations_for_n
+        self.has_equations_for_k = has_equations_for_k
+
+    def get_density(self):
+        return self.density
+
+    def set_density(self, density):
+        self.density = density
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def make_list_from_file(path):
+        """ Takes in the file path of a CSV with each entry organised as
+            [wavelength],[n/k] and forms a list of tuples. If any tuples are a
+            different size to any of the others, will raise a ValueError.
+            If the file is not a CSV, will also raise a ValueError.
+
+            Perhaps future implementation for tab separated files?
+        """
+        f = open(absolute_path, 'r')
+        entries = f.readlines()
+        f.close()
+        for entry in entries:
+            float(entry.strip().split(','))
+        list.sort(entries)      # Sort in ascending order in terms of wavelength
+        return entries
+
+    def set_n_list(self, absolute_path):
+        """ Takes in a string which is the ABSOLUTE (relative should work too,
+            but just to be safe) path of the file that contains the data for n.
+
+            File should be a CSV with each entry organised as:
+            [wavelength],[n]
+
+            If any of the entries have a length that is not 2, raises a ValueError.
+            WAVELENGTHS ARE IN MICROMETRES (this is the most convenient here,
+            and it seems refractiveindex.info tends to record in microns as well
+            if any data is taken from there)
+        """
+        self.n_list = make_list_from_file(absolute_path)
+
+    def get_n(self):
+        """ If an equation needs to be used, it will use an equation. Each
+            equation in materials_equations is identified by the material name
+            (and if there is an equation for n and k, the 'n' and 'k' string
+            identifiers in the second argument helps to differentiate)
+        """
+        if has_equations_for_n:
+            n = use_equation(self, 'n', wavelength)
+        else:
+            n = interpolate_from_list(self.n_list, wavelength)
+        return n
+
+    def get_n_list(self):
+        return self.n
+
+    def set_k_list(self, absolute_path):
+        """ Same as above, but for k
+        """
+        self.k_list = make_list_from_file(absolute_path)
+
+    def get_k(self):
+        if has_equations_for_k:
+            k = use_equation(self, 'k', wavelength)
+        else:
+            k = interpolate_from_list(self.k_list, wavelength)
+        return k
+
+    def get_k_list(self):
+        return self.k_list
+
+    def get_has_equations_for_n(self):
+        return has_equations_for_n
+
+    def set_has_equations_for_n(self, bool):
+        has_equations_for_n = bool
+
+    def get_has_equations_for_k(self):
+        return has_equations_for_n
+
+    def set_has_equations_for_k(self, bool):
+        has_equations_for_n = bool
+
+    # def save_material(self):
+    #    """Saves the material properties to a file for ease of use in the future"""
+    #    TO DO
+    #
+    # def load_material(material_name):
+    #    """Loads the material properties from a file based on the material_name (string) given"""
+    #    TO DO
+
+
+
+
+    # Possible implementation in future?
+    # def set_optical_constants(self, absolute_path):
+    #     """ Takes in a string which is the ABSOLUTE path of the file that contains
+    #         the data for BOTH n AND k. If the user has a separate file for each,
+    #         please see methods:
+    #             - set_n(self, absolute_path)
+    #             - set_k(self, absolute_path)
+    #
+    #         This file should be a CSV so that each entry is organised as:
+    #         [wavelength],[n],[k]
+    #
+    #         If any of the entries have a length that is not 3, raises a
+    #         ValueError. Ensure the wavelength is in
+    #     """
---
+++ Material/material_equations.py
@@ -0,0 +1,93 @@
+import scipy
+import numpy as np
+
+""" Any equations that are required to describe the optical constants of a given
+    material should be included here (when a table of values cannot be used).
+    Existing implementations of equations are used here as examples. If these
+    equations are only useful in a specific wavelength range, see the
+    material_helpers.use_equation() method for further details on implementation
+"""
+
+def SiO2_equations(wavelength):
+
+""" Returns refractive index of SiO_2 from wavelength argument.
+    Input wavelength is in metres. Sellmeier equation is only valid
+    from 1-8 micrometres, but that is good enough for calculating the
+    reflectivity of a silica-based lightsail for the laser bandwidth.
+    Source: Rei Kitamura, Laurent Pilon and Miroslaw Jonasz (2007).
+
+    Past Sellmeier range, use Kitamura, Pilon, Jonasz's eqn for 7-50 um
+"""
+
+    """ Nested function defining some functions required to find the complex
+        refractive index of silica. Wavenumber is in cm^-1 """
+
+    def silica_g(wavenumber, is_kkg = False):
+
+        # Quickly defining another function that is requred later on
+        def D(x):
+            D = [None]*len(x)
+            i = 0
+            while i < len(x):
+                d = scipy.special.dawsn(x[i])
+                D[i] = (d)
+                i += 1
+            return np.array(D)
+
+        alpha = np.array((3.7998, 0.46089, 1.2520, 7.8147, 1.0313, 5.3757, 6.3305, 1.2948))
+        wavenumber0 = np.array((1089.7, 1187.7, 797.78, 1058.2, 446.13, 443, 465.80, 1026.7))
+        sigma = np.array((31.454, 100.46, 91.601, 63.153, 275.111, 45.220, 22.680, 232.14))
+
+        if is_kkg == True:
+            g = (2/pi)*alpha*( D( 2 * np.sqrt(np.log(2)) * ((wavenumber+wavenumber0)/sigma) ) - D( 2 * np.sqrt(np.log(2)) * ((wavenumber-wavenumber0)/sigma) ) )
+        else:
+            g = alpha*(np.exp(-4*np.log(2)*((wavenumber-wavenumber0)/sigma)**2) - np.exp(-4*np.log(2)*((wavenumber+wavenumber0)/sigma)**2))
+        return g
+
+    # Assume k negligible below 7 microns; use Sellmeier equation
+
+    if wavelength < 7e-6:
+        sellmeier_wl = (wavelength*1e6)**2
+        term_a = (0.6961663*sellmeier_wl)/(sellmeier_wl-(0.0684043)**2)
+        term_b = (0.4079426*sellmeier_wl)/(sellmeier_wl-0.1162414**2)
+        term_c = (0.8974794*sellmeier_wl)/(sellmeier_wl-9.896161**2)
+        n = np.sqrt(1 + term_a + term_b + term_c)
+    # k no longer negligible
+    elif wavelength >= 7e-6:
+        wavenumber = (1/wavelength)/100     # in cm^-1
+        sum = 0
+        g_kkg = silica_g(wavenumber, True)
+        g = silica_g(wavenumber)
+        n = np.sqrt(2.1232 + np.sum(g_kkg) + 1j*np.sum(g))
+    return n
+
+def GeO2_Sellmeier(wavelength):
+
+""" Provides a Sellmeier equation for GeO2 which is accurate below 5 microns
+    Source: Fleming, J. W., 1984
+"""
+
+    A1 = 0.80686642
+    A2 = 0.71815848
+    A3 = 0.85416831
+    l1 = 0.68972606e-1
+    l2 = 0.15396605
+    l3 = 0.11841931e2
+    wlsq = (wavelength)**2
+    nsq = 1 + (wlsq*A1)/(wlsq-l1**2) + (wlsq*A2)/(wlsq-l2**2) + (wlsq*A3)/(wlsq-l3**2)
+    return np.sqrt(nsq)
+
+def Si3N4_Sellmeier(wavelength):
+
+""" Provides a Sellmeier equation for Si3N4 which is accurate below ~1.5 microns
+    Source: Luke et al., 2015
+"""
+
+    wavelength = wavelength
+    A1 = 3.0249
+    A2 = 40314
+    l1 = 0.1353406
+    l2 = 1239.842
+    wlsq = (wavelength)**2
+    nsq = 1 + (wlsq*A1)/(wlsq-l1**2) + (wlsq*A2)/(wlsq-l2**2)
+    return np.sqrt(nsq)
