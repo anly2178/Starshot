1ce43634cc2e7917478dede43d0610c48d260933
Andrew Ly <anly2178@uni.sydney.edu.au>
2020-08-17 13:44:38 +1000

    Added flag for gap test file

--- testfiles/material_tests/init_gap.py
+++ testfiles/material_tests/init_gap.py
@@ -1,7 +1,7 @@
 from Starshot.materials.material import Material
 
 #Initialise gap. Saves gap.pkl into saved_materials directory.
-gap = Material(name="gap", density=0, max_temp=float('inf'), abs_coeff=0, n_list_path = 'n_gap.txt', k_list_path = 'k_gap.txt')
+gap = Material(name="gap", density=0, max_temp=float('inf'), abs_coeff=0, n_list_path = ('n_gap.txt',2), k_list_path = ('k_gap.txt',2))
 
 #Print variables.
 gap.print_variables()
